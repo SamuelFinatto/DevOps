e5a42173a5243a4f258c8e1cbf30f95fdf74b39c
Samuel Finatto <br99170@aelsec.local>
2021-12-22 10:36:48 -0300

    include tests
    Signed-off-by: Samuel Finatto <br99170@aelsec.local>

---
+++ mvc.Tests/UnitTest1.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace mvc.Tests
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void TestMethod1()
+		{
+			Assert.AreEqual(1, 1);
+			Assert.AreEqual(2, 2);
+			Assert.AreEqual(3, 3);
+			Assert.AreEqual(4, 4);
+			Assert.AreEqual(5, 5);
+		}
+
+		[TestMethod]
+		public void TestMethod2()
+		{
+			Assert.AreEqual(1, 1);
+			Assert.AreEqual(2, 2);
+			Assert.AreEqual(3, 3);
+			Assert.AreEqual(4, 4);
+			Assert.AreEqual(5, 5);
+		}
+	}
+}
